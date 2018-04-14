dd4c825bf5720dfe8e5bc21613a8c054f40a7330
Piroro-hs <Piroro-hs@users.noreply.github.com>
2018-04-14 13:57:43 +0900

    [main] 💥

---
+++ main/include/controller.h
@@ -0,0 +1,26 @@
+#ifndef __CONTROLLER_H__
+#define __CONTROLLER_H__
+
+#include <stddef.h>
+#include <stdint.h>
+#include <vector>
+
+#include <mbed.h>
+
+class Controller {
+  public:
+    Controller(CAN&, const uint32_t);
+    struct {int8_t x; int8_t y; int8_t z; int8_t rz;} axes = {};
+    std::vector<bool> buttons;
+    void setButtonEventListener(Callback<void(size_t, bool)>);
+  private:
+    CAN& can;
+    const uint32_t canId;
+    //uint8_t raw[8] = {};
+    //bool received = false;
+    Callback<void(size_t, bool)> buttonCallback;
+    void recieveData();
+    void parse(const uint8_t*, const size_t);
+};
+
+#endif
---
+++ main/include/main.h
@@ -0,0 +1,8 @@
+#ifndef __MAIN_H__
+#define __MAIN_H__
+
+int main();
+
+void onUserButtonPressed();
+
+#endif
